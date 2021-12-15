07ea2d6878327722441b89e9f5ffe6dd3196873c
eshwarsundar <northeasteditsofficial@gmail.com>
2021-12-16 01:38:26 +0530

    Add message bar

---
+++ components/MessageBar/MessageBar.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import styles from "./MessageBar.module.scss";
+export const MessageBar = () => {
+  const [isClosed, setIsClosed] = useState<boolean>(false);
+  const getDisplayState = () => (isClosed ? "0" : "1");
+  return (
+    <div className={styles.messageBar} style={{ opacity: getDisplayState() }}>
+      <div className={styles.messageBar__message}>
+        If you find the website good, feel free to reuse it. Made with 💝 using
+        NextJS.{" "}
+        <a
+          href="https://github.com/eshwarne/next-portfolio"
+          className={styles.link}
+          target="_blank"
+        >
+          SOURCE CODE
+        </a>
+      </div>
+      <div
+        className={styles.messageBar__close_btn}
+        onClick={() => setIsClosed(true)}
+      >
+        <a>X</a>
+      </div>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import About from "../components/About";
 import Home from "../components/Home";
+import MessageBar from "../components/MessageBar";
 import Navbar from "../components/Navbar";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
@@ -15,6 +16,7 @@ const Portfolio: NextPage = () => {
         <meta name="description" content="Eshwar Sundar's portfolio website" />
         <link rel="icon" href="/picture-self.jpeg" />
       </Head>
+      <MessageBar />
       <Navbar />
       <div className={"pageContainer"}>
         <section>
