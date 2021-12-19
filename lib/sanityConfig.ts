bf7e0f67e734f8a1120002f853db952546b9fdfd
eshwarsundar <northeasteditsofficial@gmail.com>
2021-12-19 22:28:02 +0530

    update readme

--- lib/sanityConfig.ts
+++ lib/sanityConfig.ts
@@ -1,4 +1,4 @@
-const sanityConfig = {  // Find your project ID and dataset in `sanity.json` in your studio project
+const sanityConfig = { 
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
     useCdn: process.env.NODE_ENV === 'production',
