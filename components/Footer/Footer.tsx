78e68d5fccf4ad3393f58f33c40b821b11996ffa
eshwarsundar <northeasteditsofficial@gmail.com>
2021-12-19 22:56:37 +0530

    Add tech footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,17 +4,23 @@ import Tony from "../../public/tony_dog.jpeg";
 export const Footer = () => {
   return (
     <div className={styles.footer}>
-      <div className={styles.footer__text}>
-        Thanks for going through my website! I really appreciate it! Here is a
-        photo of my dog Tony 🐕, celebrating his birthday, to make your day! 🥳
+      <div className={styles.footer__main}>
+        <div className={styles.footer__text}>
+          Thanks for going through my website! I really appreciate it! Here is a
+          photo of my dog Tony 🐕, celebrating his birthday, to make your day!
+          🥳
+        </div>
+        <div className={styles.footer__image}>
+          <Image
+            alt="Tony, the golden retriever"
+            layout="fill"
+            src={Tony}
+            objectFit="cover"
+          />
+        </div>
       </div>
-      <div className={styles.footer__image}>
-        <Image
-          alt="Tony, the golden retriever"
-          layout="fill"
-          src={Tony}
-          objectFit="cover"
-        />
+      <div className={styles.footer__tech}>
+        Made with 🤍 with NextJS and Sanity.IO
       </div>
     </div>
   );
